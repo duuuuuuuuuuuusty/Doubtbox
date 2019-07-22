3d5348f75b67dfe1cd7c0bc4153ded16bc000e28
Dusty <deltac.sierra@gmail.com>
2019-07-22 18:40:45 -0400

    Create Doubtbox Wrapper.js
    Append before wrapper script, before trigger script

---
+++ JS/Doubtbox Wrapper.js
@@ -0,0 +1,5 @@
+let avatarElem = document.createElement('div');
+avatarElem.innerHTML = `<!-- |avatar| -->`;
+window.doubtboxAvatar = avatarElem.querySelector('img').getAttribute('src');
+window.doubtboxPreferences = '<!-- |field_20| -->';
+window.doubtboxAuth = '<!-- |auth_key| -->';
